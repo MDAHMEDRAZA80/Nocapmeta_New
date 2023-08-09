6f148acb63e176f43927632c942c062d37adee2e
MDAHMEDRAZA80 <ahmedraza8292@gmail.com>
2023-08-09 14:41:15 +0530

    first commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://nocapmeta-project-backend.onrender.com",
+  },
+};
+
+export default config;
